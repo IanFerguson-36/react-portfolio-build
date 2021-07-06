3f7a403a8c8d40f443872790938d7148829ee453
Ian F <ian.ferg36@gmail.com>
2021-07-06 15:20:22 -0600

    added snap scroll and hid scrollbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import Intro from "./components/Intro/Intro";
 import Portfolio from "./components/Portfolio/Portfolio";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Works from "./components/Works/Works";
+import "./App_.scss";
 
 function App() {
   return (
     <div className="app">
       <Topbar />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
