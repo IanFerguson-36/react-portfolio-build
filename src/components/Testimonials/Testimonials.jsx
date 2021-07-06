86f3e51fd13e4a04ac05534cb5222c63c01dc56e
Ian F <ian.ferg36@gmail.com>
2021-07-06 15:45:37 -0600

    added empty ids to components

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import "./Testimonials.scss"
 
 export default function Testimonials() {
     return (
-        <div className= "testimonials">
+        <div className= "testimonials" id="testimonials">
             
         </div>
     )
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -4,6 +4,14 @@ import "./Topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
+            <div clasName="wrapper">
+                <div className="left">
+                    <a href="#intro"></a>
+                </div>
+                <div className="right"></div>
+                
+
+            </div>
             
         </div>
     )
