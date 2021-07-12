59e43e9589e13b7127a4fee6e9a5783b7e5e3d09
Ian F <ian.ferg36@gmail.com>
2021-07-12 17:13:04 -0600

    added transition and transform to hamburger

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -4,7 +4,7 @@ import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
-       <div className="topbar">
+       <div className="topbar active">
         <div className="wrapper">
            <div className="left">
                <a href="#intro" className="logo">Smort.</a>
