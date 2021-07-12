470a0a0fa663cc471d74430ba51133b78a1d8a50
Ian F <ian.ferg36@gmail.com>
2021-07-12 16:46:58 -0600

    created hamburger menu

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Topbar.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Topbar() {
     return (
@@ -7,8 +8,23 @@ export default function Topbar() {
         <div className="wrapper">
            <div className="left">
                <a href="#intro" className="logo">Smort.</a>
+               <div className="itemcontainer">
+                    <Person className="icon"/>
+                <span>678-447-9596</span>
+             </div>
+             <div className="itemcontainer">
+                 <Mail className="icon"/>
+                 <span>ian.ferg36@gmail.com</span>
+             </div>
            </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="hamburger">
+                   <div className="line"></div>
+                   <div className="line"></div>
+                   <div className="line"></div>
+        
+                </div>
+            </div>
         </div>
      </div>
     );
